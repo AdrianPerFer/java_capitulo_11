7417234eaac2ad7153009ceb00146ab6f8a53b43
AdrianPerFer <chopon007@gmail.com>
2023-02-01 23:06:11 +0100

    Ejercicio2 added

---
+++ Ejercicio2/Ejercicio2.java
@@ -0,0 +1,14 @@
+package Ejercicio2;
+
+/**
+ * Realiza un programa que lea el fichero creado en el ejercicio anterior y que muestre los números por pantalla.
+ * 
+ * @author Adrián Perogil Fernández
+ */
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        LeerFichero leer = new LeerFichero();
+        leer.leer();
+    }
+}
---
+++ Ejercicio2/LeerFichero.java
@@ -0,0 +1,25 @@
+package Ejercicio2;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class LeerFichero {
+    public void leer(){
+        String linea = "";
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("primos.dat"));
+            linea = br.readLine();
+            while (linea !=null) {
+                System.out.print(linea + " ");
+                linea = br.readLine();
+            }
+            System.out.println();
+            br.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
